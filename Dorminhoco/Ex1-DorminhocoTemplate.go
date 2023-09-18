b2552f6ce6902a322d336807eff5b00bba6ef0a8
kaitowing <pizzolatti.arthur@edu.pucrs.br>
2023-09-17 23:18:53 -0300

    Exercicio 10 quase feito

--- Dorminhoco/Ex1-DorminhocoTemplate.go
+++ Dorminhoco/Ex1-DorminhocoTemplate.go
@@ -8,47 +8,58 @@
 //   O ultimo leva a rolhada ...
 //   ESTE  PROGRAMA NAO FUNCIONA.    É UM RASCUNHO COM DICAS.
 
-
 package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
-const NJ = 5           // numero de jogadores
-const M = 4            // numero de cartas
+const NJ = 5 // numero de jogadores
+const M = 3  // numero de cartas
 
-type carta string      // carta é um strirng
+type carta string // carta é um strirng
 
-var ch [NJ]chan carta  // NJ canais de itens tipo carta  
+var ch [NJ]chan carta // NJ canais de itens tipo carta
 
-func horaDeBater(cartas []carta) bool {
-	 if (cartas[0] == cartas[1] && cartas[1] == cartas[2] && cartas[2] == cartas[3]) {
-		 return true
-	 } else {
-		 return false
-	 }
+func horaDeBater(cartas []carta, id int) bool {
+	if cartas[0] == cartas[1] && cartas[1] == cartas[2] && cartas[2] == cartas[3] {
+		return true
+	} else {
+		return false
+	}
 }
 
 func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta, batida chan bool) {
 	mao := cartasIniciais    // estado local - as cartas na mao do jogador
-	nroDeCartas := M         // quantas cartas ele tem 
-  cartaRecebida := " "     // carta recebida é vazia
+	nroDeCartas := M         // quantas cartas ele tem
+	if id == 0 {
+		nroDeCartas = 4
+	}
+	fmt.Println(id, "começou")
 
 	for {
-		select{
+		select {
 		case bateu := <-batida:
-			fmt.Println(id, " bateu")
+			fmt.Println(id, "bateu")
+			batida <- bateu
 			return
+		case cartaRecebida := <-in:
+		//	fmt.Println(id, "recebe carta")
+			mao[nroDeCartas] = cartaRecebida
+			nroDeCartas++
+		//	fmt.Println(id, "Numero de cartas: ", nroDeCartas)
 		default:
-			fmt.Println(id, " joga")
-			if (nroDeCartas == 4) {
-				out <- mao[3]
-			} else {
-				if(horaDeBater(mao)){
+			if nroDeCartas == 4 {
+			//	fmt.Println(id, "joga")
+				randomIndex := rand.Intn(nroDeCartas-1) // Gera um índice aleatório
+				out <- mao[randomIndex]
+				// Move a carta jogada para a última posição e atualiza nroDeCartas
+				mao[randomIndex] = mao[nroDeCartas-1]
+				nroDeCartas--
+				if (horaDeBater(mao, id)) {
 					batida <- true
-				} else {
-					out <- cartaRecebida
+					return
 				}
 			}
 		}
@@ -56,17 +67,25 @@ func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta, bati
 }
 
 func main() {
-	
+	batida := make(chan bool)
+
 	for i := 0; i < NJ; i++ {
-		ch[i] = make(chan struct{})
+		ch[i] = make(chan carta)
 	}
 	// cria um baralho com NJ*M cartas
 	for i := 0; i < NJ; i++ {
 		// escolhe aleatoriamente (tira) cartas do baralho, passa cartas para jogador
-		go jogador(i, ch[i], ch[(i+1)%N], cartasEscolhidas , batida) // cria processos conectados circularmente
+		cartasEscolhidas := make([]carta, M+1)
+		for j := 1; j < M+1; j++ {
+			cartasEscolhidas[j] = carta(string(rune(j)))
+		}
+
+		if i == 0 {
+			cartasEscolhidas[3] = carta(string(rand.Intn(2) + 1))
+		}
+
+		go jogador(i, ch[i], ch[(i+1)%NJ], cartasEscolhidas, batida) // cria processos conectados circularmente
 	}
-	
-	<-make(chan struct{}) // bloqueia
+
+	<-make(chan bool)
 }
-
-
